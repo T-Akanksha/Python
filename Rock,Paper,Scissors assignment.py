f7ecaeb7507894444967f5bae7f522dc1b03959e
T-Akanksha <118503687+T-Akanksha@users.noreply.github.com>
2022-11-18 16:27:59 +0530

    Add files via upload

---
+++ Rock,Paper,Scissors assignment.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+p1=input("enter your choice ")
+p2=input("enter your choice ")
+rock=1
+paper=2
+scissors=3
+if p1=="rock" and p2=="rock" or p1=="paper" and p2=="paper" or p1=="scissors" and p2=="scissors":
+    print("play again same")
+elif p1=="rock" and p2=="paper" or p1=="paper" and p2=="scissors" or p1=="scissors" and p2=="rock":
+    print("p2 wins")
+elif p1=="rock" and p2=="scissors" or p1=="paper" and p2=="rock" or p1=="scissors" and p2=="paper":
+    print("p1 wins")
+else:
+    print("written mistake")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
