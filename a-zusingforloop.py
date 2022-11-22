c8296b7230765b85bdae9b1e0aa30a95a9d49cb3
T-Akanksha <118503687+T-Akanksha@users.noreply.github.com>
2022-11-22 15:30:02 +0530

    Add files via upload

---
+++ a-zusingforloop.py
@@ -0,0 +1,493 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for a in range(1,4):
+    if a==1:
+        print(" *******")
+    elif a==2:
+        print("*        *")
+    elif a==3:
+        print(" ******* * *  *")
+    a=a+1
+
+
+# In[5]:
+
+
+for a in range(1,7):
+    if a==1:
+        print("*")
+    elif a==2:
+        print("*        ")
+    elif a==3:
+        print("*********")
+    elif a==4:
+        print("*       *")
+    elif a==5:
+        print("*       *")
+    elif a==6:
+        print("*********")
+    a=a+1
+
+
+# In[7]:
+
+
+for a in range(1,5):
+    if a==1:
+        print("  **********")
+    elif a==2:
+        print("*        ")
+    elif a==3:
+        print("*")
+    elif a==4:
+        print(" ***********")
+    a=a+1
+
+
+# In[10]:
+
+
+for a in range(1,6):
+    if a==1:
+        print("           *")
+    elif a==2:
+        print("           *")
+    elif a==3:
+        print("************")
+    elif a==4:
+        print("*          *")
+    elif a==5:
+        print("************")
+    a=a+1
+
+
+# In[11]:
+
+
+for a in range(1,6):
+    if a==1:
+        print("************")
+    elif a==2:
+        print("*          *")
+    elif a==3:
+        print("************")
+    elif a==4:
+        print("*          ")
+    elif a==5:
+        print("************")
+    a=a+1
+
+
+# In[17]:
+
+
+for a in range(1,8):
+    if a==1:
+        print("     ************")
+    elif a==2:
+        print("     *          *")
+    elif a==3:
+        print("     ************")
+    elif a==4:
+         print("     *")
+    elif a==5:
+        print("  *****************")
+    elif a==6:
+        print("     *")
+    elif a==7:
+        print("     *")
+    a=a+1
+
+
+# In[19]:
+
+
+for a in range(1,6):
+    if a==1:
+        print("     ************")
+    elif a==2:
+        print("     *          *")
+    elif a==3:
+        print("     ************")
+    elif a==4:
+         print("                * ")
+    elif a==5:
+        print("     ************")
+    a=a+1
+
+
+# In[24]:
+
+
+for a in range(1,6):
+    if a==1:
+        print("     *")
+    elif a==2:
+        print("     *          ")
+    elif a==3:
+        print("     ************")
+    elif a==4:
+         print("     *          * ")
+    elif a==5:
+        print("     *          *")
+    a=a+1
+
+
+# In[14]:
+
+
+for a in range(1,8):
+    if a==1:
+        print("      *  *")
+    elif a==2:
+        print("    * * *  ")
+    elif a==3:
+        print("     ")
+    elif a==4:
+         print("      *")
+    elif a==5:
+        print("      *")
+    elif a==6:
+        print("      *")
+    elif a==7:
+        print("      *")
+    a=a+1
+
+
+# In[16]:
+
+
+for a in range(1,8):
+    if a==1:
+        print("      *  *")
+    elif a==2:
+        print("    * * *  ")
+    elif a==3:
+        print("     ")
+    elif a==4:
+         print("      *")
+    elif a==5:
+        print("*     *")
+    elif a==6:
+        print("*     *")
+    elif a==7:
+        print("  * *   ")
+    a=a+1
+
+
+# In[17]:
+
+
+for a in range(1,6):
+    if a==1:
+        print("*      *")
+    elif a==2:
+        print("*   *")
+    elif a==3:
+        print("* *")
+    elif a==4:
+        print("*   *")
+    elif a==5:
+        print("*      *")
+    a=a+1
+
+
+# In[19]:
+
+
+for a in range(1,8):
+    if a==1:
+        print("      *  ")
+    elif a==2:
+        print("      *  ")
+    elif a==3:
+        print("      *")
+    elif a==4:
+         print("      *")
+    elif a==5:
+        print("      *")
+    elif a==6:
+        print("      *")
+    elif a==7:
+        print("      *")
+    a=a+1
+
+
+# In[22]:
+
+
+for a in range(1,8):
+    if a==1:
+        print("      *        *  *                  *    * ")
+    elif a==2:
+        print("      *     *        *           *          *")
+    elif a==3:
+        print("      *  *            *      *               *")
+    elif a==4:
+         print("      *                   *                   *")
+    elif a==5:
+        print("      *                                        *")
+    elif a==6:
+        print("      *                                        *")
+    elif a==7:
+        print("      *                                        *")
+    a=a+1
+
+
+# In[23]:
+
+
+for a in range(1,8):
+    if a==1:
+        print("      *        *  *    ")
+    elif a==2:
+        print("      *     *        *  ")
+    elif a==3:
+        print("      *  *            * ")
+    elif a==4:
+         print("      *               *")
+    elif a==5:
+        print("      *                *")
+    elif a==6:
+        print("      *                *")
+    elif a==7:
+        print("      *                *")
+    a=a+1
+
+
+# In[26]:
+
+
+for a in range(1,7):
+    if a==1:
+        print("       *   *    ")
+    elif a==2:
+        print("    *        *  ")
+    elif a==3:
+        print("   *          *  ")
+    elif a==4:
+        print("   *          * ")
+    elif a==5:
+        print("    *        * ")
+    elif a==6:
+        print("       *  * ")
+    a=a+1
+
+
+# In[27]:
+
+
+for a in range(1,7):
+    if a==1:
+        print("***********")
+    elif a==2:
+        print("*         *")
+    elif a==3:
+        print("*         *")
+    elif a==4:
+        print("**********")
+    elif a==5:
+        print("*")
+    elif a==6:
+        print("*")
+    a=a+1
+    
+
+
+# In[29]:
+
+
+for a in range(1,7):
+    if a==1:
+        print("***********")
+    elif a==2:
+        print("*         *")
+    elif a==3:
+        print("*         *")
+    elif a==4:
+        print("**********      *")
+    elif a==5:
+        print("         *   *")
+    elif a==6:
+        print("         *")
+    a=a+1
+
+
+# In[36]:
+
+
+for a in range(1,7):
+    if a==1:
+        print("    *           *")
+    elif a==2:
+        print("      *       *")
+    elif a==3:
+        print("       *    *")
+    elif a==4:
+        print("        * *")
+    elif a==5:
+        print("      *     *   ")
+    elif a==6:
+        print("         *")
+    a=a+1
+
+
+# In[38]:
+
+
+for a in range(1,6):
+    if a==1:
+        print("**********")
+    elif a==2:
+        print("*         ")
+    elif a==3:
+        print("**********")
+    elif a==4:
+        print("         *")
+    elif a==5:
+        print("**********")
+    a=a+1
+        
+        
+
+
+# In[40]:
+
+
+for a in range(1,6):
+    if a==1:
+        print("     *")
+    elif a==2:
+        print("     *         ")
+    elif a==3:
+        print("**********")
+    elif a==4:
+        print("     *      *")
+    elif a==5:
+        print("       ****")
+    a=a+1
+        
+        
+
+
+# In[37]:
+
+
+for a in range(1,7):
+    if a==1:
+        print("    *         *    ")
+    elif a==2:
+        print("   *          *  ")
+    elif a==3:
+        print("   *          *  ")
+    elif a==4:
+        print("   *          * ")
+    elif a==5:
+        print("    *        *  * ")
+    elif a==6:
+        print("       *  *       *")
+    a=a+1
+
+
+# In[44]:
+
+
+for a in range(1,7):
+    if a==1:
+        print("   *         *    ")
+    elif a==2:
+        print("   *         *  ")
+    elif a==3:
+        print("   *         *  ")
+    elif a==4:
+        print("    *       * ")
+    elif a==5:
+        print("     *     *   ")
+    elif a==6:
+        print("        *     ")
+    a=a+1
+
+
+# In[45]:
+
+
+for a in range(1,7):
+    if a==1:
+        print("*              *  ")
+    elif a==2:
+        print("*              *  ")
+    elif a==3:
+        print("*              *  ")
+    elif a==4:
+        print("*       *      * ")
+    elif a==5:
+        print("*   *       *  *")
+    elif a==6:
+        print("*              * ")
+    a=a+1
+
+
+# In[46]:
+
+
+for a in range(1,6):
+    if a==1:
+        print("*               *  ")
+    elif a==2:
+        print("   *         *     ")
+    elif a==3:
+        print("        *       ")
+    elif a==4:
+        print("    *       *  ")
+    elif a==5:
+        print("*               * ")
+    a=a+1
+
+
+# In[50]:
+
+
+for a in range(1,7):
+    if a==1:
+        print("*         *")
+    elif a==2:
+        print("*         *")
+    elif a==3:
+        print("***********")
+    elif a==4:
+        print("         *")
+    elif a==5:
+        print("*       *")
+    elif a==6:
+        print("    *    ")
+    a=a+1
+    
+        
+
+
+# In[51]:
+
+
+for a in range(1,6):
+    if a==1:  
+        print("       *********** ")
+    elif a==2:
+        print("                 * ")
+    elif a==3:
+        print("             *")
+    elif a==4:
+        print("         *")
+    elif a==5:
+        print("        ***********")
+    a=a+1
+
+
+# In[ ]:
+
+
+
+
